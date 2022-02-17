05a997f42407757db3b1f31346b150e965422f37
Christopher de Oliveira <christopherforwork@gmail.com>
2022-02-17 10:04:21 -0400

    Introduction to states, loops, conditionals and events

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,54 @@
 <script>
-	export let name;
+	let showText = false;
+	
+	let firstName = 'Christopher';
+	let lastName = 'de Oliveira';
+	let color = 'blue';
+
+	let users = [
+		{
+			id: 1,
+			name: 'John',
+		},
+		{
+			id: 2,
+			name: 'Sara',
+		},
+		{
+			id: 3,
+			name: 'Bob',
+		},
+	];
+
+	$: name = firstName + ' ' + lastName;
+
+	const toggle = () => {
+		color = color === 'blue' ? 'red' : 'blue';
+		showText = !showText;
+		users = [
+			...users,
+			{
+				id: 4,
+				name: 'Jen',
+			},
+		]
+	}
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1 style="color: {color}">Hello {name}!</h1>
+
+	{#if showText}
+		<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	{:else}
+		<p>No Text</p>
+	{/if}
+
+	<button on:click={toggle}>Click</button>
+
+	{#each users as user (user.id)}
+		<h3>{user.id}: {user.name}</h3>
+	{/each}
 </main>
 
 <style>
