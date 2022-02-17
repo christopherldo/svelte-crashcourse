ee9bec99ca82a363fc450506669cfe04d3d9e146
Christopher de Oliveira <christopherforwork@gmail.com>
2022-02-17 11:28:03 -0400

    Created Button and FeedbackForm components

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,5 @@
 <script>
+	import FeedbackForm from './components/FeedbackForm.svelte';
 	import FeedbackList from './components/FeedbackList.svelte';
 	import FeedbackStats from './components/FeedbackStats.svelte';
 
@@ -32,6 +33,7 @@
 </script>
 
 <main class="container">
+	<FeedbackForm />
 	<FeedbackStats {count} {average} />
 	<FeedbackList {feedback} on:delete-feedback={deleteFeedback} />
 </main>
---
+++ src/components/FeedbackForm.svelte
@@ -0,0 +1,64 @@
+<script>
+  import Card from './Card.svelte';
+  import Button from './Button.svelte';
+
+  let text = '';
+  let min = 10;
+
+  $: btnDisabled = text.trim().length < min;
+  $: errorMessage = text.trim().length < min && text.trim().length != 0 ? `Text must be at least ${min} characters long` : '';
+</script>
+
+<Card>
+  <header>
+    <h2>How would you rate your service with us?</h2>
+  </header>
+  <form>
+    <!-- Rating Select -->
+    <div class="input-group">
+      <input type="text" bind:value={text} placeholder="Tell us something that keeps you coming back">
+      <Button disabled={btnDisabled} type="submit">Send</Button>
+    </div>
+    {#if errorMessage}
+      <div class="message">{errorMessage}</div>
+    {/if}
+  </form>
+</Card>
+
+<style>
+  header {
+    max-width: 400px;
+    margin: auto;
+  }
+
+  header h2 {
+    font-size: 22px;
+    font-weight: 600;
+    text-align: center;
+  }
+
+  .input-group {
+    display: flex;
+    flex-direction: row;
+    border: 1px solid #ccc;
+    padding: 8px 10px;
+    border-radius: 8px;
+    margin-top: 15px;
+  }
+
+  input {
+    flex-grow: 2;
+    border: none;
+    font-size: 16px;
+  }
+
+  input:focus {
+    outline: none;
+  }
+
+  .message{
+    padding-top: 10px;
+    text-align: center;
+    color: rebeccapurple;
+  }
+</style>
